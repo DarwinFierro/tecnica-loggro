cad066719198785ca7e1bbc917e7b5ad364b0879
DarwinFierro <darwinfrsoftware@gmail.com>
2025-09-06 00:50:07 -0500

    feat(test): se agregan los test unitarios por parametros

--- src/test/java/com/notPerfectBot/TecnicaLoggro/TecnicaLoggroApplicationTests.java
+++ src/test/java/com/notPerfectBot/TecnicaLoggro/TecnicaLoggroApplicationTests.java
@@ -1,13 +1,38 @@
 package com.notPerfectBot.TecnicaLoggro;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TecnicaLoggroApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @ParameterizedTest(name = "Palabra: {0} → esperado: {1}")
+    @CsvSource({
+            "A, true",
+            "LIBRO, true",
+            "BOZO, false",
+            "TRAJE, true",
+            "COMUN, true",
+            "CAMPANA, false",
+            "DORITO, true",
+            "ARLEQUIN, true"
+    })
+    void testEndpointPostConCasos(String palabra, boolean esperado) throws Exception {
+        mockMvc.perform(post("/api/bloques/")
+                        .contentType("application/json")
+                        .content("{\"palabra\":\"" + palabra + "\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Boolean.toString(esperado)));
+    }
 }
