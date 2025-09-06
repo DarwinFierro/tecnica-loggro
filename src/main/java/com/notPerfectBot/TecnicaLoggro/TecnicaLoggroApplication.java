63167dc75af155cee50f44a4738a1157fe735e76
DarwinFierro <darwinfrsoftware@gmail.com>
2025-09-06 03:28:51 -0500

    fix(Cors): se agregan cors rapidas para lectura de frontend

--- src/main/java/com/notPerfectBot/TecnicaLoggro/TecnicaLoggroApplication.java
+++ src/main/java/com/notPerfectBot/TecnicaLoggro/TecnicaLoggroApplication.java
@@ -2,6 +2,10 @@ package com.notPerfectBot.TecnicaLoggro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class TecnicaLoggroApplication {
@@ -9,5 +13,18 @@ public class TecnicaLoggroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TecnicaLoggroApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:[0-9]*")
+                        .allowedMethods("POST")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
